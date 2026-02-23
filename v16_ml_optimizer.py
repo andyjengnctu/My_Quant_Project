ee8089b40ee4fff6814cc09fa923ce1a0bd3f980
andyjengnctu <andyjengnctu@gmail.com>
2026-02-23 21:46:17 +0800

    test changes

--- v16_ml_optimizer.py
+++ v16_ml_optimizer.py
@@ -79,7 +79,7 @@ def objective(trial):
         kc_mult = trial.suggest_float("kc_mult", 1.5, 3.0, step=0.1) if ai_use_kc else 2.0,
         
         vol_short_len = trial.suggest_int("vol_short_len", 3, 10) if ai_use_vol else 5,
-        vol_long_len = trial.suggest_int("vol_long_len", 15, 50) if ai_use_vol else 19
+        vol_long_len = trial.suggest_int("vol_long_len", 10, 30) if ai_use_vol else 19 #changed
     )
 
     all_stats = []
